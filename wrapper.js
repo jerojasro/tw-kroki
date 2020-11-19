8a01847e1327c44097bd0d374c9e4aa41e281a6a
Javier Eduardo Rojas Romero <jerojasro@devnull.li>
2020-11-19 14:33:41 -0500

    change content type for this plugin, remove unused markdown stuff

--- wrapper.js
+++ wrapper.js
@@ -3,7 +3,8 @@ title: $:/plugins/jerojasro/plantuml/wrapper.js
 type: application/javascript
 module-type: parser
 
-Wraps up the remarkable parser for use as a Parser in TiddlyWiki
+Generates a new type of parse node, plantuml, to be processed by the plantuml
+widget: $:/plugins/jerojasro/plantuml/widget.js
 
 \*/
 (function(){
@@ -12,43 +13,10 @@ Wraps up the remarkable parser for use as a Parser in TiddlyWiki
 /*global $tw: false */
 "use strict";
 
-var r = require("$:/plugins/jerojasro/plantuml/remarkable.js");
-
-var Remarkable = r.Remarkable,
-	linkify = r.linkify,
-	utils = r.utils;
-
-///// Set up configuration options /////
-function parseAsBoolean(tiddlerName) {
-	return $tw.wiki.getTiddlerText(tiddlerName).toLowerCase() === "true";
-}
-var pluginOpts = {
-	linkNewWindow: parseAsBoolean("$:/config/plantuml/linkNewWindow"),
-	renderWikiText: parseAsBoolean("$:/config/plantuml/renderWikiText"),
-	renderWikiTextPragma: $tw.wiki.getTiddlerText("$:/config/plantuml/renderWikiTextPragma").trim()
-};
-var remarkableOpts = {
-	breaks: parseAsBoolean("$:/config/plantuml/breaks"),
-	quotes: $tw.wiki.getTiddlerText("$:/config/plantuml/quotes"),
-	typographer: parseAsBoolean("$:/config/plantuml/typographer")
-};
-var accumulatingTypes = {
-	"text": true,
-	"softbreak": true
-};
-
-var md = new Remarkable(remarkableOpts);
-
-if (parseAsBoolean("$:/config/plantuml/linkify")) {
-	md = md.use(linkify);
-}
-
-var MarkdownParser = function(type, text, options) {
-	//this.tree = [{"type": "raw", "html": req.responseText}];
-
+var PlantumlParser = function(type, text, options) {
 	this.tree = [{"type": "plantuml", "html": "<h1>holi</h1>"}];
 };
 
-exports["text/x-markdown"] = MarkdownParser;
+exports["text/x-plantuml"] = PlantumlParser;
 
 })();
